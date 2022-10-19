d47a3a316b17adcd0031941460742d726bd2bbbb
Mikko Vuorinen <vuorinem@gmail.com>
2022-10-19 23:41:42 +0100

    Add a portfolio-page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { NextPage } from "next"
+import Link from "next/link"
+import styles from './index.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Mikko Vuorinen</title>
-        <meta name="description" content="Portfolio and personal website of software developer Mikko Vuorinen" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <h1 className={styles.title}>
+                Welcome to my website!
+            </h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Mikko Vuorinen
-        </h1>
-
-        <p className={styles.description}>
-          Welcome to my website!
-        </p>
-      </main>
-
-      <footer className={styles.footer}>
-        TODO: Links to Twitter, LinkedIn etc
-      </footer>
-    </div>
-  )
+            <p className={styles.description}>
+                <Link href="/portfolio"><a>Here is a lists some of my website projects.</a></Link>
+            </p>
+        </>
+    )
 }
 
 export default Home
