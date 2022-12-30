d5f76a1ca1fd2f7907a3e36abd7f7b41201c7a90
Mikko Vuorinen <vuorinem@gmail.com>
2022-12-30 19:28:04 +0000

    Add icon to help identify external links

--- components/external-link.tsx
+++ components/external-link.tsx
@@ -1,9 +1,10 @@
 import Link, { LinkProps } from "next/link";
-import { CSSProperties, FunctionComponent, PropsWithChildren } from "react";
+import { FunctionComponent, PropsWithChildren } from "react";
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 const ExternalLink: FunctionComponent<PropsWithChildren<LinkProps>> = (props) => {
     return (
-        <Link target="_blank" rel="noreferrer noopener nofollow" {...props}>{props.children}</Link>
+        <Link target="_blank" rel="noreferrer noopener nofollow" {...props}>{props.children} <FaExternalLinkAlt size="0.6rem" /></Link>
     );
 }
 
