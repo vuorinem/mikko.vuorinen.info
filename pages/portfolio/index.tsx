3fe0ea35226bc44798c2cdf4725d8d6474dadb7e
Mikko Vuorinen <vuorinem@gmail.com>
2022-12-30 16:51:14 +0000

    Split portfolio projects by  type

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -10,32 +10,49 @@ type PortfolioProps = {
 }
 
 const Portfolio: NextPage<PortfolioProps> = ({ projects }) => {
-    const projectsElement = projects.map(project => {
-        return (
-            <div key={project.name} className={styles.project}>
-                <div className={styles.info}>
-                    <h2>{project.title}</h2>
-                    {project.text ? <p dangerouslySetInnerHTML={{ __html: project.text }}></p> : ""}
-                    {project.link ? <p><ExternalLink href={project.link}>{project.link.replace(/https?:\\\\/, "")}</ExternalLink></p> : ""}
+    const GetProjectsList = (type: string) => projects
+        .filter(project => project.type === type)
+        .map(project => {
+            return (
+                <div key={project.name} className={styles.project}>
+                    <div className={styles.info}>
+                        <h2>{project.title}</h2>
+                        {project.text ? <p dangerouslySetInnerHTML={{ __html: project.text }}></p> : ""}
+                        {project.link ? <p><ExternalLink href={project.link}>{project.link.replace(/https?:\\\\/, "")}</ExternalLink></p> : ""}
+                    </div>
+                    <div className={styles.screenshot}>
+                        <Image
+                            src={`/portfolio/images/${project.name}.png`}
+                            alt={`Screenshot from ${project.title}`}
+                            fill />
+                    </div>
                 </div>
-                <div className={styles.screenshot}>
-                    <Image
-                        src={`/portfolio/images/${project.name}.png`}
-                        alt={project.title}
-                        fill />
-                </div>
-            </div>
-        )
-    });
+            );
+        });
 
     return (
         <>
             <h1>Projects</h1>
+
+            <h2>Projects in full-time employment</h2>
             <div className={styles.projects}>
-                {projectsElement}
+                {GetProjectsList('employed')}
+            </div>
+
+            <h2>Freelance projects</h2>
+            <p>
+                These are some of the project I have worked on as a freelance web developer
+            </p>
+            <div className={styles.projects}>
+                {GetProjectsList('freelance')}
+            </div>
+
+            <h2>Personal projects and studies</h2>
+            <div className={styles.projects}>
+                {GetProjectsList('personal')}
             </div>
         </>
-    )
+    );
 }
 
 export const getStaticProps: GetStaticProps<PortfolioProps> = () => {
