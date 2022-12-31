d7ba950a1db81b63900d6911725a8f24974e1979
Mikko Vuorinen <vuorinem@gmail.com>
2022-12-31 20:25:11 +0000

    Add page transition animations, style improvements and some text content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,41 @@
 import { NextPage } from "next"
 import Link from "next/link"
+import ExternalLink from "../components/external-link"
 
 import styles from './index.module.css'
 
 const Home: NextPage = () => {
     return (
-        <>
-            <h1 className={styles.title}>
-                Welcome to my website!
-            </h1>
+        <div className={styles.home}>
+            <p className={styles.description}>
+                Hi, I'm a software engineer from Finland with a passion for
+                learning every day, and applying what I have learned to create
+                the best possible software, teams, organisations and experiences.
+            </p>
 
             <p className={styles.description}>
-                <Link href="/portfolio">▸ Some of my website projects</Link>
+                I strive for ecologically sustainable and a socially just future
+                where we respect and celebrate diversity.
             </p>
-        </>
+
+            <ul className={styles.links}>
+                <li>▸ See <Link href="/portfolio">my portfolio</Link> to get an idea what kind of projects I have done</li>
+                <li>▸ For my work experience and education, head to <ExternalLink href="https://www.linkedin.com/in/mikkovuorinen/">my LinkedIn page</ExternalLink></li>
+            </ul>
+
+            <p className={styles.description}>
+                Outside software world, I practice
+                {' '}<ExternalLink href="https://en.wikipedia.org/wiki/Kendo">kendo</ExternalLink>{' '}
+                several times a week as one of the coaches at my club. I especially enjoy competing
+                in kendo.
+            </p>
+
+            <p className={styles.description}>
+                Music has always been close to my heart, and while I no longer actively play
+                any instrument, listening to music is a key element to set the mood for anything
+                I do. I hope to create and play more music in the future.
+            </p>
+        </div>
     )
 }
 
