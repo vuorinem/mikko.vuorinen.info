58e72e3c64395355ce06137924cf414f3d04c15b
Mikko Vuorinen <vuorinem@gmail.com>
2023-01-15 00:16:45 +0000

    Add new projects and re-organise portfolio page

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,16 +1,16 @@
 import { GetStaticProps, NextPage } from "next"
 import Image from "next/image"
-import { Project } from "../../data/portfolio/Project"
-import projects from "../../data/portfolio/projects.json"
+import { Project, ProjectCollection } from "../../data/portfolio/Project"
+import projectCollection from "../../data/portfolio/projects.json"
 import ExternalLink from "../../components/external-link"
 
 import styles from './index.module.css'
 
 type PortfolioProps = {
-    projects: Project[]
+    projectCollection: ProjectCollection
 }
 
-const Portfolio: NextPage<PortfolioProps> = ({ projects }) => {
+const Portfolio: NextPage<PortfolioProps> = ({ projectCollection }) => {
     const GetTechTags = (tech: string[]) => tech
         .sort((a, b) => a.localeCompare(b, 'en'))
         .map(techName => {
@@ -19,22 +19,21 @@ const Portfolio: NextPage<PortfolioProps> = ({ projects }) => {
             );
         });
 
-    const GetProjectsList = (type: string) => projects
-        .filter(project => project.type === type)
+    const GetProjectsList = (projects: Project[]) => projects
         .map(project => {
             return (
                 <div key={project.name} className={styles.project}>
-                    <div className={styles.info}>
-                        <h2>{project.title}</h2>
-                        {project.text ? <p>{project.text}</p> : ""}
-                        {project.link ? <p><ExternalLink href={project.link}>{project.link.replace(/https?:\\\\/, "")}</ExternalLink></p> : ""}
-                        {project.tech ? <p>{GetTechTags(project.tech)}</p> : ""}
-                    </div>
                     <div className={styles.screenshot}>
                         <Image
-                            src={`/portfolio/images/${project.name}.png`}
-                            alt={`Screenshot from ${project.title}`}
+                            src={`/portfolio/images/${project.image}`}
+                            alt={`${project.imageDescription}`}
                             fill />
+                    </div>
+                    <div className={styles.info}>
+                        <h2>{project.title}</h2>
+                        {project.text ? <p className={styles.projectText}>{project.text}</p> : ""}
+                        {project.link ? <p className={styles.projectLink}><ExternalLink href={project.link}>{project.link.replace(/https?:\\\\/, "")}</ExternalLink></p> : ""}
+                        {project.tech ? <p className={styles.projectTech}>{GetTechTags(project.tech)}</p> : ""}
                     </div>
                 </div>
             );
@@ -44,23 +43,37 @@ const Portfolio: NextPage<PortfolioProps> = ({ projects }) => {
         <>
             <h1>Projects</h1>
 
-            <h2>Projects in full-time employment</h2>
+            <h2>Ongoing personal projects</h2>
             <div className={styles.projects}>
-                {GetProjectsList('employed')}
+                {GetProjectsList(projectCollection.ongoing)}
             </div>
 
-            <h2>Freelance projects</h2>
+            <h2>Past projects in full-time employment</h2>
+            <div className={styles.projects}>
+                {GetProjectsList(projectCollection.employed)}
+            </div>
+
+            <h2>Past freelance projects</h2>
             <p>
                 These are some of the project I have worked on as a freelance web developer
             </p>
             <div className={styles.projects}>
-                {GetProjectsList('freelance')}
+                {GetProjectsList(projectCollection.freelance)}
             </div>
 
-            <h2>Personal projects and studies</h2>
+            <h2>Publications and presentations</h2>
+            <p>
+                Various publications and presentations done as part of employment, studies or free time.
+            </p>
             <div className={styles.projects}>
-                {GetProjectsList('personal')}
+                {GetProjectsList(projectCollection.publication)}
             </div>
+
+            <p className={styles.attributions}>
+                Attributions:
+                Photo by Pixabay: https://www.pexels.com/photo/qr-code-on-screengrab-278430/ |
+                Photo by Proxyclick Visitor Management System: https://www.pexels.com/photo/cellphone-and-scanner-2451622/ |
+            </p>
         </>
     );
 }
@@ -68,7 +81,7 @@ const Portfolio: NextPage<PortfolioProps> = ({ projects }) => {
 export const getStaticProps: GetStaticProps<PortfolioProps> = () => {
     return {
         props: {
-            projects
+            projectCollection
         }
     }
 }
