c122e4c74a009e68ffae25ade94c0ff4ab8f7070
Mikko Vuorinen <vuorinem@gmail.com>
2023-10-05 01:10:27 +0100

    Fix typos on front page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       </p>
 
       <ul className={styles.links}>
-        <li>▸ See <Link href="/portfolio">my portfolio</Link> to get an idea what kind of projects I have done</li>
+        <li>▸ See <Link href="/portfolio">my portfolio</Link> to get an idea of what kind of projects I have done</li>
         <li>▸ Read the <Link href="/narrative-cv">narrative CV</Link> to learn the story of my career so far</li>
         <li>▸ Take a look at the case studies of <Link href="/narrative-cv#casestudy-siriusreader">Sirius Reader</Link>, <Link href="/narrative-cv#casestudy-lahjalista">Lahjalista.net</Link>, <Link href="/narrative-cv#casestudy-opeka">Opeka</Link> and <Link href="/narrative-cv#casestudy-riskmanagement">a risk management application</Link></li>
         <li>▸ For my work experience and education, head to <ExternalLink href="https://www.linkedin.com/in/mikkovuorinen/">my LinkedIn page</ExternalLink></li>
@@ -26,7 +26,7 @@ export default function Home() {
       </ul>
 
       <p className={styles.description}>
-        Outside software world, I practice
+        Outside the software world, I practice
         {' '}<ExternalLink href="https://en.wikipedia.org/wiki/Kendo">kendo</ExternalLink>{' '}
         several times a week as one of the coaches at my club. I especially enjoy competing
         in kendo.
